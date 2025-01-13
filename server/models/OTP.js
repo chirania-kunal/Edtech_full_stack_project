dd8fc26a4500b349e54d8457cb1fd7459fd2e0a0
chirania-kunal <chiraniakunal@gmail.com>
2025-01-13 08:33:36 +0530

    pre middleware for sending Mails

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -18,16 +18,20 @@ const OTPSchema = new mongoose.Schema({
 });
 
 // a function to send emails
-// async function sendVerificationEmail(email, otp){
-//     try{
-//         const mailResponse = await mailSender(email, "Verification Email from StudyNotion", otp);
-//         console.log("Email Sent Successfully: ",mailResponse);
-//     }catch(error){
-//         console.log("error occurred while sending emails: ", error);
-//         throw error;
-//     }
-// }
+async function sendVerificationEmail(email, otp){
+    try{
+        const mailResponse = await mailSender(email, "Verification Email from StudyNotion", otp);
+        console.log("Email Sent Successfully: ",mailResponse);
+    }catch(error){
+        console.log("error occurred while sending emails: ", error);
+        throw error;
+    }
+}
 
-// OTPSchema.pre("save", async)
+// pre- save middleware
+OTPSchema.pre("save", async function(next) {
+    await sendVerificationEmail(this.email ,this.otp);
+    next();
+})
 
 module.exports = mongoose.model("OTP",OTPSchema);
